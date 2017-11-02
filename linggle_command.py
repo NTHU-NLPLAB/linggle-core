964d2b8bd4dfdeec52991dfe9dbfa9052ceb9572
JJC <jjc@nlplab.cc>
2017-11-02 17:56:06 +0800

    to old format function

--- linggle_command.py
+++ linggle_command.py
@@ -3,6 +3,8 @@
 from itertools import product
 
 from pos_table import has_pos
+from math import ceil, log10
+from operator import itemgetter
 
 
 LONGEST_LEN = 5
@@ -87,6 +89,23 @@ def convert_to_nopos_query(querystr):
 def satisfy_pos_condition(ngram, condition):
     ngram = ngram.split(' ')
     return all(has_pos(ngram[i], pos.lower()) for i, pos in condition)
+
+
+def to_old_linggle_format(result):
+    def roundsig(number, digit=2):
+        return int(round(number, -int(ceil(log10(number)))+digit))
+
+    total = sum(map(itemgetter(1), result)) / 100
+    old_format_result = []
+    for ngram, count in result:
+        old_format_result.append({
+            'count': int(count),
+            'phrase': ngram,
+            'count_str': '{0:,}'.format(roundsig(count)),
+            'percent': '{0}%'.format(round(count / total, 1))
+        })
+
+    return old_format_result
 
 
 def find_synonyms(word):
