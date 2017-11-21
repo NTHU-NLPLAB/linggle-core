dfea2e177c0da3d3fea1d8d0fdf2fc96de0dfb13
JJC <jjc@nlplab.cc>
2017-11-21 17:05:28 +0800

    pos module

--- linggle/pos/pos_table.py
+++ linggle/pos/pos_table.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 from collections import defaultdict
 
-PREPROSITIONS = set(line.strip() for line in open('pg.prep.txt'))
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+PREPROSITION_FILE_PATH = os.path.join(MODULE_PATH, 'pg.prep.txt')
+PREPROSITIONS = set(line.strip() for line in open(PREPROSITION_FILE_PATH))
+
+POSTABLE_PATH = os.path.join(MODULE_PATH, 'postable.norm.txt')
 
 
 def init_postable():
     postable = defaultdict(set)
-    for line in open('postable.norm.txt'):
+    for line in open(POSTABLE_PATH):
         word, pos = line.strip('"\n\r ').split('"\t"')
         postable[pos].add(word)
     return postable
