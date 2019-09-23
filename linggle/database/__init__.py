d47a08069dab7208cb48f852b1449d5e5aec1a01
JJC <jjc@nlplab.cc>
2019-09-23 12:45:26 +0800

    add linggle for english, chinese

--- linggle/database/__init__.py
+++ linggle/database/__init__.py
@@ -1,2 +1,28 @@
-from .linggle_cassandra import CassandraLinggle  # noqa
-from .linggle_postgres import PostgresLinggle  # noqa
+from .linggle import BaseLinggle, DbLinggle, NoPosDbLinggle
+from .linggle_cassandra import CassandraLinggle
+from .linggle_postgres import PostgresLinggle
+
+
+__all__ = ['BaseLinggle', 'DbLinggle', 'NoPosDbLinggle', 'CassandraLinggle', 'PostgresLinggle']
+
+
+class EnLinggle(BaseLinggle):
+    """English Version of Linggle"""
+    def __init__(self, *args, **kwargs):
+        self.word_delimiter = ' '
+        super().__init__(*args, **kwargs)
+
+
+class ZhLinggle(BaseLinggle):
+    """Chinese Version of Linggle"""
+    def __init__(self, *args, **kwargs):
+        self.word_delimiter = ''
+        super().__init__(*args, **kwargs)
+
+
+class Web1tLinggle(EnLinggle, CassandraLinggle, NoPosDbLinggle):
+    """For web1t, the language is english, PoS is not included and we use cassandra as our dbms"""
+
+
+class UcdLinggle(ZhLinggle, PostgresLinggle):
+    """For udn and cna, the language is chinese and we use postgres as our dbms"""
