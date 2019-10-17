98ed8e6a64c3b99c0b887fa9a662da2717d3a17c
JJC <jjc@JJCdeMacBook-Pro.local>
2019-10-17 15:58:06 +0800

    rewrite ngram count map

--- linggle/process/ngramcount_map.py
+++ linggle/process/ngramcount_map.py
@@ -11,19 +11,14 @@ def ngram_is_valid(ngram):
     return all(token not in black_list for token in ngram)
 
 
-def to_ngrams(items, length):
-    return zip(*(items[i:] for i in range(length)))
-
-
 def gen_ngrams(items, max_len=5):
-    for i in range(1, max_len+1):
-        for ngram in to_ngrams(items, i):
+    for n in range(1, max_len+1):
+        for ngram in zip(*(items[i:] for i in range(n))):
             yield ngram
 
 
 def main(iterable):
-    lines = map(str.strip, iterable)
-    for line in lines:
+    for line in map(str.strip, iterable):
         ngrams = gen_ngrams(line.split())
         for ngram in filter(ngram_is_valid, ngrams):
             print(*ngram)
