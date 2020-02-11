cf20c5c5f1e0b1ecb0886c0f1e345d716bcd6a97
JJC <jjc@nlplab.cc>
2020-02-11 13:49:14 +0800

    feat: update BNC pos table with enchant

--- linggle/pos/pos_table.py
+++ linggle/pos/pos_table.py
@@ -2,24 +2,26 @@
 # -*- coding: utf-8 -*-
 import os
 
+from .abbr import POS_WILDCARD
 
 # Build paths inside the project like: os.path.join(BASE_DIR, ...)
 MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
+POS_FILE_PATH = os.path.join(MODULE_PATH, 'data', 'bnc_pos.txt')
+PREP_FILE_PATH = os.path.join(MODULE_PATH, 'data', 'prepositions.txt')
 
 
 def init_postable():
-    postable = {}
-    postable_path = os.path.join(MODULE_PATH, 'data', 'bnc_postable.txt')
-    for line in open(postable_path):
-        word, pos = line.strip('"\n\r ').split('"\t"')
-        postable.setdefault(pos, set()).add(word)
-    prep_file_path = os.path.join(MODULE_PATH, 'data', 'prepositions.txt')
-    postable['prep'] = set(open(prep_file_path).read().split())
+    postable = {abbr: set() for abbr in POS_WILDCARD.values()}
+    for line in open(POS_FILE_PATH):
+        word, *poss = line.strip().split()
+        for pos in poss:
+            postable[f"{pos}."].add(word)
+    postable['p.'] = set(open(PREP_FILE_PATH).read().split())
     return postable
 
 
 def has_pos(word, pos):
-    return word in POS_TABLE[pos]
+    return pos in POS_TABLE and word in POS_TABLE[pos]
 
 
 POS_TABLE = init_postable()
