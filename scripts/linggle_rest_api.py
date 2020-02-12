293734e99b9c8736f7ef9cc3ae0fa27a63c0de70
JJC <jjc@nlplab.cc>
2020-02-12 15:54:01 +0800

    fix: res overwritten by str in POST and reuse get function

--- scripts/linggle_rest_api.py
+++ scripts/linggle_rest_api.py
@@ -20,18 +20,12 @@ app = FastAPI()
 
 @app.get("/ngram/{cmd}", response_class=UJSONResponse)
 def get_ngram(cmd: str, time: int = None):
-    cmd = cmd.strip()
-    ngrams = linggle_it(cmd) if cmd else []
-    total = sum(map(itemgetter(-1), ngrams))
-    res = {'query': cmd, 'result': ngrams, 'total': total}
-    if time:
-        res['time'] = time
-    return res
+    return get_ngram_post(LinggleQuery(query=cmd, time=time))
 
 
 @app.post("/ngram/", response_class=UJSONResponse)
 def get_ngram_post(res: LinggleQuery):
-    res = res.query.strip()
+    res.query = res.query.strip()
     res.result = linggle_it(res.query) if res.query else []
     res.total = sum(map(itemgetter(-1), res.result))
     return res
