7974f93c62674f95c1085f7ad21d9f958af9604b
JJC <jjc@nlplab.cc>
2020-02-25 14:33:13 +0800

    feat: add logging in the linggle datase

--- scripts/linggle_database.py
+++ scripts/linggle_database.py
@@ -1,10 +1,13 @@
 import os
+import logging
+
 
 _backend = os.getenv('LINGGLE_BACKEND', 'web1t')
 if _backend == 'web1t':
     from linggle.database import Web1tLinggle as Linggle
 elif _backend == 'zhpg':
     from linggle.database import ZhPgLinggle as Linggle
+
 
 RETRY_TIMES = 3
 linggle = None
@@ -14,7 +17,8 @@ def linggle_it(query):
     for _ in range(RETRY_TIMES):
         try:
             return linggle[query]
-        except Exception:
+        except Exception as e:
+            logging.error(str(e))
             init_linggle()
     return []
 
