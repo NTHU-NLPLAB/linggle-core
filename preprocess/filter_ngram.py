9d0a7bec7fb8c3fa7200b4975d1bd294283b6a9f
JJC <jjc@nlplab.cc>
2020-03-25 17:50:54 +0800

    feat: a simple version of filter ngram

--- preprocess/filter_ngram.py
+++ preprocess/filter_ngram.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
+
+ITEM_RE = re.compile(r'\(([^()]+)\)?')
+
 numbers = set('0123456789０１２３４５６７８９')
 eng_symbols = set('{}"\'()[].,:;+!?-*/&|<>=~$%\n\t ')
 ch_symbols = set('｛｝「」『』【】（）〔〕，。：；＋！？﹖——＊７｜＜＞《》〈〉＝～＄％、')
 black_list = numbers | eng_symbols | ch_symbols
 
 
-def text_till(text, sep='(', start=0):
-    i = text.find(sep, start)
-    return '' if i < 0 else text[:i]
-
-
 def ngram_is_valid(ngram):
-    return all(text_till(token, start=1).strip() not in black_list for token in ngram)
+    return all(ITEM_RE.findall(token) for token in ngram)
 
 
 if __name__ == "__main__":
