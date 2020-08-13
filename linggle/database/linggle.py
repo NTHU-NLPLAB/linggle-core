0c53bd99963f0553aef736fa65477de699925c50
JJC <jjc@nlplab.cc>
2020-08-13 10:52:24 +0800

    feat: remove workaround for ngrams with special symbols causing wrong length

--- linggle/database/linggle.py
+++ linggle/database/linggle.py
@@ -112,6 +112,4 @@ class NoPosLinggle(BaseLinggle):
     @staticmethod
     def satisfy_conditions(row, conditions=()):
         ngram = row[0].split()
-        # TODO: remove those ngram with special space symbols so that we don't need this if-statement
-        if conditions[-1][0] < len(ngram):
-            return all(has_pos(ngram[i], condition) for i, condition in conditions)
+        return all(has_pos(ngram[i], condition) for i, condition in conditions)
