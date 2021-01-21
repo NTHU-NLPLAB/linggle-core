5ad815454b557cbb62b6b5cb66952fdea3d33f5a
JJC <jjc@nlplab.cc>
2021-01-21 21:14:21 +0800

    feat: add vocabulary module

---
+++ linggle/database/vocab.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+
+
+MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
+VOCAB_FILE_PATH = os.environ.get('VOCAB_FILE_PATH') or os.path.join(MODULE_PATH, 'vocab.txt')
+
+
+def parse_line(line):
+    word, count = line.strip().split('\t')
+    return word, int(count)
+
+
+if Path(VOCAB_FILE_PATH).is_file():
+    VOCABULARY = dict(map(parse_line, open(VOCAB_FILE_PATH)))
+else:
+    VOCABULARY = {}
